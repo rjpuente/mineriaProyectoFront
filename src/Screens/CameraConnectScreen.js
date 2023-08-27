e9dffc6d69e6d0c835c32558a5bb84dce52aa094
Rodrigo Puente <rjpuente@uce.edu.ec>
2023-08-27 01:26:21 -0500

    Cambios varios

--- src/Screens/CameraConnectScreen.js
+++ src/Screens/CameraConnectScreen.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import { View, Button, Picker } from "react-native";
+import io from "socket.io-client";
 
 const CameraConnectScreen = () => {
   const [connected, setConnected] = useState(false);
   const [devices, setDevices] = useState([]);
   const [selectedDevice, setSelectedDevice] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  const webcamRef = useRef(null); // Referencia a la webcam
 
   useEffect(() => {
     // Obtener la lista de dispositivos de entrada disponibles
@@ -18,6 +22,33 @@ const CameraConnectScreen = () => {
   const handleConnect = () => {
     // Conectar a la cámara seleccionada
     setConnected(true);
+
+    // Establecer la conexión WebSocket
+    const newSocket = io("http://localhost:5000"); // Cambia la URL a tu backend
+    setSocket(newSocket);
+  };
+
+  useEffect(() => {
+    if (socket) {
+      // Escuchar eventos desde el backend
+      socket.on("prediction", (prediction) => {
+        console.log("Prediction received:", prediction);
+        // Aquí puedes mostrar la notificación o realizar otras acciones según la predicción
+      });
+
+      socket.on("error", (error) => {
+        console.error("Error received:", error);
+      });
+    }
+  }, [socket]);
+
+  const handleSendImage = () => {
+    if (socket && selectedDevice && webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+
+      // Enviar la imagen al backend a través del socket
+      socket.emit("image", imageSrc);
+    }
   };
 
   return (
@@ -45,7 +76,8 @@ const CameraConnectScreen = () => {
         </View>
       ) : (
         <View>
-          <Webcam audio={false} videoConstraints={{ deviceId: selectedDevice }} />
+          <Webcam audio={false} videoConstraints={{ deviceId: selectedDevice }} ref={webcamRef} />
+          <Button title="Enviar Imagen" onPress={handleSendImage} />
         </View>
       )}
     </View>
