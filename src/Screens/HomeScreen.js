3656ecf5c8b605674985caa5004f267d436ec753
Rodrigo Puente <rjpuente@uce.edu.ec>
2023-08-31 23:17:13 -0500

    Cambios en home screen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,64 +1,88 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Webcam from 'react-webcam';
-import { View, Text, Button, Picker, StyleSheet, TouchableOpacity } from 'react-native';
-import io from 'socket.io-client';
-import { COLORS } from '../constants';
+import React, { useState, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+import { View, Picker, Button, StyleSheet, Text } from "react-native";
+import io from "socket.io-client";
 
 const CameraConnectScreen = () => {
   const [connected, setConnected] = useState(false);
   const [devices, setDevices] = useState([]);
-  const [selectedDevice, setSelectedDevice] = useState('');
+  const [selectedDevice, setSelectedDevice] = useState("");
   const [socket, setSocket] = useState(null);
+  const [suspiciousActivity, setSuspiciousActivity] = useState(false);
 
-  const webcamRef = useRef(null); // Referencia a la webcam
+  const webcamRef = useRef(null);
 
   useEffect(() => {
-    // Obtener la lista de dispositivos de entrada disponibles
     navigator.mediaDevices.enumerateDevices().then((devices) => {
-      const cameras = devices.filter((device) => device.kind === 'videoinput');
+      const cameras = devices.filter((device) => device.kind === "videoinput");
       setDevices(cameras);
     });
   }, []);
 
-  const handleConnect = () => {
-    // Conectar a la cámara seleccionada
-    setConnected(true);
+  useEffect(() => {
+    if (socket) {
+      const imageSendingInterval = setInterval(() => {
+        handleSendImage();
+      }, 5000);
 
-    // Establecer la conexión WebSocket
-    const newSocket = io('http://localhost:5000'); // Cambia la URL a tu backend
-    setSocket(newSocket);
-  };
+      return () => {
+        clearInterval(imageSendingInterval);
+      };
+    }
+  }, [socket]);
 
   useEffect(() => {
     if (socket) {
-      // Escuchar eventos desde el backend
-      socket.on('prediction', (prediction) => {
-        console.log('Prediction received:', prediction);
-        // Aquí puedes mostrar la notificación o realizar otras acciones según la predicción
-      });
+      socket.on("prediction", (response) => {
+        console.log("Prediction received:", response);
 
-      socket.on('error', (error) => {
-        console.error('Error received:', error);
+        // Verificar si el class_index es diferente de 3
+        if (response.class_index == 3 ||  response.class_index == 6) {
+          setSuspiciousActivity(false);
+        } else {
+          setSuspiciousActivity(true);
+        }
       });
     }
   }, [socket]);
 
-  const handleSendImage = () => {
-    if (socket && selectedDevice && webcamRef.current) {
-      const imageSrc = webcamRef.current.getScreenshot();
+  const handleConnect = () => {
+    setConnected(true);
+    const newSocket = io("http://localhost:5000");
+    setSocket(newSocket);
+    console.log("Connected successfully", newSocket);
+  };
 
-      // Enviar la imagen al backend a través del socket
-      socket.emit('image', imageSrc);
+  const handleSendImage = () => {
+    console.log("Sending image");
+    if (socket && selectedDevice && webcamRef.current) {
+      const newImageSrc = webcamRef.current.getScreenshot();
+
+      fetch(newImageSrc)
+        .then((response) => response.blob())
+        .then((blob) => {
+          if (blob.size > 0) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+              const imageData = event.target.result;
+              socket.emit("image", imageData); // Enviar la cadena base64
+            };
+            reader.readAsDataURL(blob);
+          } else {
+            console.warn("Image blob is empty");
+          }
+        })
+        .catch((error) => {
+          console.error("Error sending image:", error);
+        });
     }
   };
 
   return (
-    <View style={styles.container}>
-    <Text style={styles.title}>Cámara</Text>
+    <View>
       {!connected ? (
-        <View style={styles.formContainer}>
+        <View>
           <Picker
-            style={[styles.picker, styles.input]}
             selectedValue={selectedDevice}
             onValueChange={(value) => setSelectedDevice(value)}
           >
@@ -71,25 +95,26 @@ const CameraConnectScreen = () => {
               />
             ))}
           </Picker>
-          
-          <TouchableOpacity
+          <Button
+            title="Conectar a la cámara"
             onPress={handleConnect}
             disabled={!selectedDevice}
-            style={[
-              styles.connectButton,
-              !selectedDevice && styles.disabledButton,
-            ]}
-          >
-            <Text style={styles.buttonText}>Conectar a la cámara</Text>
-          </TouchableOpacity>
-
+          />
         </View>
       ) : (
         <View style={styles.webcamContainer}>
-          <Webcam audio={false} videoConstraints={{ deviceId: selectedDevice }} ref={webcamRef} />
-          <TouchableOpacity style={styles.connectButton} onPress={handleSendImage}>
-            <Text style={styles.buttonText}>Enviar Imagen</Text>
-          </TouchableOpacity>
+          <Webcam
+            audio={false}
+            videoConstraints={{ deviceId: selectedDevice }}
+            ref={webcamRef}
+          />
+          {suspiciousActivity && (
+            <View style={styles.popup}>
+              <Text style={styles.suspiciousMessage}>
+                ¡Actividad sospechosa detectada!
+              </Text>
+            </View>
+          )}
         </View>
       )}
     </View>
@@ -99,70 +124,23 @@ const CameraConnectScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.WHITE,
-  },
-  title: {
-    fontSize: 25,
-    marginTop: 20,
-    marginBottom: 20,
-    color: COLORS.GREEN,
   },
   webcamContainer: {
-    alignItems: 'center',
+    flex: 1,
   },
-  formContainer: {
-    alignItems: 'center',
-  },
-  picker: {
-    width: '100%',
-    height: 50,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  sendImageButton: {
-    marginTop: 10,
-    backgroundColor: COLORS.SECONDARY,
-    color: COLORS.WHITE,
+  popup: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "red",
     paddingVertical: 10,
     paddingHorizontal: 20,
   },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  connectButton: {
-    marginTop: 20,
-    backgroundColor: COLORS.GREEN,
-    width: '100%',
-    height: 48,
-    borderRadius: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 20,
-    shadowColor: COLORS.GREEN,
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 10
-  },
-  disabledButton: {
-    backgroundColor: COLORS.GREEN_SOFT
-  },
-  buttonText: {
-    color: COLORS.WHITE,
+  suspiciousMessage: {
+    color: "white",
     fontSize: 16,
-    textAlign: 'center'
+    textAlign: "center",
   },
 });
 
