4a433ef5064e4992bf4a37e5c5c4ed57048c50d1
Rodrigo Puente <rjpuente@uce.edu.ec>
2023-09-03 19:43:05 -0500

    Cambios en tiempo y visualización

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import { View, Picker, Button, StyleSheet, Text } from "react-native";
 import io from "socket.io-client";
-import axios from 'axios';
+import axios from "axios";
 
 const HomeScreen = () => {
   const [connected, setConnected] = useState(false);
@@ -10,8 +10,12 @@ const HomeScreen = () => {
   const [selectedDevice, setSelectedDevice] = useState("");
   const [socket, setSocket] = useState(null);
   const [suspiciousActivity, setSuspiciousActivity] = useState(false);
-  const registrationToken1 = 'fS3iQ0H_Rsu1uuILxtYhV9:APA91bEXLeq-bHbFVgiNGWIs0IckjFKYrjS1PArGr8tFDeFjBnqb49k_Za8Nw1ZkwltLzo06FWP7PJygKumUUcdoyiU8tvMlHghQ_AfJr5DPreT4kDweJ8zgNhGVVlvYib7aiMMB-qLg'; 
-  const registrationToken2 = 'eOXMWqQiR9yyNQjAR3YszP:APA91bE1hVISel447vDx2_6IB9tCEpbO1AlfpzgEQl9s-P8dQKCu8PCHtidCSE2kig1ys41kFJHad21W1EzVRxN12yOF8mcrhOrqB-JbVTXMJuHXA3QyqfilHqv9P2f_6aoAPs4ZytML';
+  const registrationToken1 =
+    "fS3iQ0H_Rsu1uuILxtYhV9:APA91bEXLeq-bHbFVgiNGWIs0IckjFKYrjS1PArGr8tFDeFjBnqb49k_Za8Nw1ZkwltLzo06FWP7PJygKumUUcdoyiU8tvMlHghQ_AfJr5DPreT4kDweJ8zgNhGVVlvYib7aiMMB-qLg";
+  const registrationToken2 =
+    "eOXMWqQiR9yyNQjAR3YszP:APA91bE1hVISel447vDx2_6IB9tCEpbO1AlfpzgEQl9s-P8dQKCu8PCHtidCSE2kig1ys41kFJHad21W1EzVRxN12yOF8mcrhOrqB-JbVTXMJuHXA3QyqfilHqv9P2f_6aoAPs4ZytML";
+
+  let isSendingNotification = false;
 
   const webcamRef = useRef(null);
 
@@ -26,7 +30,7 @@ const HomeScreen = () => {
     if (socket) {
       const imageSendingInterval = setInterval(() => {
         handleSendImage();
-      }, 4000);
+      }, 500);
 
       return () => {
         clearInterval(imageSendingInterval);
@@ -38,24 +42,41 @@ const HomeScreen = () => {
     if (socket) {
       socket.on("prediction", async (response) => {
         console.log("Prediction received:", response);
-        const clase = response.class_index
-        // Verificar si el class_index es diferente de 3
-        if (response.class_index == 3 ||  response.class_index == 6) {
-          setSuspiciousActivity(false);
-        } else {
-          const notificationData = {
-            tokens: [registrationToken1,registrationToken2],
-            title: 'Evento detectado',
-            body: 'Se ha detectado un evento sospechoso',
-            categoria: response.class_index
-          };
-          try {
-            const response = await axios.post('https://verbose-dollop-g667rwgj9xwhwj6q-3000.app.github.dev/send-notification', notificationData);
-            console.log('Notification sent:', response.data.message);
-          } catch (error) {
-            console.error('Error sending notification:', error);
+        const clase = response.class_index;
+        // Verificar si el class_index es diferente de 6
+        if (response.class_index == 6) {
+          if (!isSendingNotification) {
+            setTimeout(() => {
+              isSendingNotification = false;
+            }, 6000); // Espera 5 segundos antes de permitir el próximo envío
+            setSuspiciousActivity(false);
           }
-          setSuspiciousActivity(true);
+        } else {
+          if (!isSendingNotification) {
+            isSendingNotification = true;
+
+            const notificationData = {
+              tokens: [registrationToken1, registrationToken2],
+              title: "Evento detectado",
+              body: "Se ha detectado un evento sospechoso",
+              categoria: response.class_index,
+            };
+
+            try {
+              const response = await axios.post(
+                "https://verbose-dollop-g667rwgj9xwhwj6q-3000.app.github.dev/send-notification",
+                notificationData
+              );
+              console.log("Notification sent:", response.data.message);
+            } catch (error) {
+              console.error("Error sending notification:", error);
+            }
+
+            setTimeout(() => {
+              isSendingNotification = false;
+            }, 6000); // Espera 5 segundos antes de permitir el próximo envío
+            setSuspiciousActivity(true);
+          }
         }
       });
     }
@@ -118,13 +139,13 @@ const HomeScreen = () => {
         </View>
       ) : (
         <View style={styles.container}>
-        <View style={styles.webcamContainer}>
-          <Webcam
-            audio={false}
-            videoConstraints={{ deviceId: selectedDevice }}
-            ref={webcamRef}
-          />
-        </View>
+          <View style={styles.webcamContainer}>
+            <Webcam
+              audio={false}
+              videoConstraints={{ deviceId: selectedDevice }}
+              ref={webcamRef}
+            />
+          </View>
           {suspiciousActivity && (
             <View style={styles.popup}>
               <Text style={styles.suspiciousMessage}>
@@ -144,13 +165,12 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1, // Esto asegura que el contenedor ocupe toda la pantalla
-    justifyContent: 'center', // Centra verticalmente
-    alignItems: 'center', // Centra horizontalmente
+    justifyContent: "center", // Centra verticalmente
+    alignItems: "center", // Centra horizontalmente
   },
   webcamContainer: {
     flex: 1,
     width: 780,
-
   },
   popup: {
     position: "absolute",
